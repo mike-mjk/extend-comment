17594ad299b0a34dbb3240118da9f4cea811343e
mike-mjk <mikeavida@gmail.com>
2017-08-20 15:37:27 -0700

    add App to storybook for completeness

--- client/src/stories/index.js
+++ client/src/stories/index.js
@@ -16,14 +16,22 @@ import '../bootstrap/css/bootstrap.css';
 import Message from '../components/message';
 import MessageContainer from '../components/message-container';
 import MessageInput from '../components/message-input';
+import App from '../components/App';
 
-const store = createStore(reducers); // storiesOf('Message', module).add('with text', () => <Message text="This is some text" />);
+const store = createStore(reducers); // storiesOf('Message', module).add('with text', () => <Message text="This is some text" />); // storiesOf('App', module).add('App', () => <App />);
 
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+// storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
 // storiesOf('Button', module)
 // 	.add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
 // 	.add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
+storiesOf('App', module)
+	.addDecorator(story =>
+		<Provider store={store}>
+			{story()}
+		</Provider>
+	)
+	.add('App', () => <App />);
 storiesOf('MessageContainer', module)
 	.addDecorator(story =>
 		<Provider store={store}>
