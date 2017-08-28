94b4914a42f3908eac6bbc2325c71971eca34df8
mike-mjk <michael.kostolnik@gmail.com>
2017-08-28 09:15:21 -0700

    https'

--- client/src/index.js
+++ client/src/index.js
@@ -12,7 +12,7 @@ import ApolloClient from 'apollo-client';
 import { ApolloProvider, createNetworkInterface } from 'react-apollo';
 
 const networkInterface = createNetworkInterface({
-	uri: 'http://bl-comment-api.herokuapp.com/graphql'
+	uri: 'https://bl-comment-api.herokuapp.com/graphql'
 });
 
 const client = new ApolloClient({
